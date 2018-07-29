a0aca5b619c70ba03521ea57fc86b44609d1ae79
Olivier Robert <olivierobert@gmail.com>
2018-07-29 13:06:39 +0700

    Loader for the June 2018 Session of the Nimbl3 Design Lab #2 (#2)
    * Vcliq-inspired loading animation for Nimbl3 Lab July 2018 session
    
    * Add README with the submission details of the loader
    
    * Minor improvement of the wording to describe the user flow steps
    
    * Add punctuation between the user flow steps

---
+++ 2018-july/README.md
@@ -0,0 +1,37 @@
+# Nimbl3 Design Lab - July 2018
+
+**Book** a date. A photographer **shoots** your event. Download amazing **photos**. Repeat.
+
+This loader is a short summary of the flow a user goes through when booking a photographer on [vcliq](https://vcliq.com/).
+
+The main idea is to animate a green dot through the illustrations representing each aforementioned step. The dot acts:
+
+1. The slot picked on the calendar
+
+2. The lens of the camera 
+
+3. The sun above the mountain ☀️
+
+## Resources & Tools
+
+* All animation done using [Popmotion](https://popmotion.io/)
+
+* All SVG icon design done by [Mai](https://github.com/Maibhu)
+
+---
+
+## The Instructions
+For this Design Lab session, we challenge you to design or build an eye-popping animated *loader* for either web, iOS, Android or even CLI. 
+* Type of loader: animated text, GIF or SVG.
+* Upload an image of the animated loader to Invision: https://projects.invisionapp.com/d/main#/projects/prototypes/14719701. If you built the actual loader, uploaded video on Github or link to the repository are welcome. 
+* We want to you to have as much freedom as needed. Don't hesitate to think about about new applications or techniques to bring uniqueness to your submission! It's not about doing an illustration, it's about a creative idea and thinking about cool animations.
+* There is no limit in submission per person. So you can submit as many loader as you want.
+* This needs to be custom work! So submission created by a loader generator will be disqualified.
+
+## How does the challenge work?
+* Registration & submission: July 2nd → July 27 (~3.5 weeks)
+* Voting and winner announced: July 31st
+
+## Prize
+* 2 Freestyle Sessions tickets (60 min) at Bounce INC (Emquartier): https://www.bounceinc.co.th
+* The winner will also have their work featured on the Dribbble account of Nimbl3
---
+++ 2018-july/assets/javascript/components/loader.js
@@ -0,0 +1,43 @@
+(function(popmotion) {
+  const { easing, timeline, styler } = popmotion;
+
+  const loader = document.querySelector('.loader');
+
+  const dotStyler = styler(loader.querySelector('#dot'));
+  const calendarStyler = styler(loader.querySelector('#iconCalendar'));
+  const cameraStyler = styler(loader.querySelector('#iconCamera'));
+  const imageStyler = styler(loader.querySelector('#iconImage'));
+
+  // Set initial position
+  cameraStyler.set({x: 200});
+  imageStyler.set({x: 200});
+
+  timeline([
+    { track: 'calendar', from: { opacity: 0, x: 0 }, to: { opacity: 1, x: 0 }, duration: 300, ease: easing.backIn },
+    { track: 'dot', from: { opacity: 0, scale: 0, x: 54, y: 83 }, to: { opacity: 1, scale: 1, x: 66, y: 95 }, duration: 300, ease: easing.backIn },
+    { track: 'calendar', from: { opacity: 1 }, to: { opacity: 1 }, duration: 500 },
+    { track: 'calendar', from: { x: 0 }, to: { x: -200 }, duration: 100, ease: easing.easeOut },
+    [
+      { track: 'camera', from: { x: 200 }, to: { x: 0 }, duration: 200 },
+      { track: 'dot', from: { x: 66, y: 95 }, to: { x: 87, y: 94 }, duration: 200, ease: easing.linear },
+    ],
+    { track: 'camera', from: { opacity: 1 }, to: { opacity: 1 }, duration: 500 },
+    { track: 'camera', from: { x: 0 }, to: { x: -200 }, duration: 100, ease: easing.easeOut },
+    [
+      { track: 'image', from: { x: 200 }, to: { x: 0 }, duration: 200 },
+      { track: 'dot', from: { x: 87, y: 94 }, to: { x: 50, y: 55 }, duration: 200, ease: easing.linear },
+    ],
+    { track: 'image', from: { opacity: 1 }, to: { opacity: 1 }, duration: 500 },
+    [
+      { track: 'image', from: { opacity: 1 }, to: { opacity: 0 }, duration: 200, ease: easing.easeOut },
+      { track: 'dot', from: { opacity: 1, scale: 1, x: 50, y: 55 }, to: { opacity: 0, scale: 0, x: 38, y: 43 }, duration: 300, ease: easing.easeOut },
+    ]
+  ],{
+    loop: Infinity
+  }).start(function(v) {
+    dotStyler.set(v.dot);
+    calendarStyler.set(v.calendar);
+    cameraStyler.set(v.camera);
+    imageStyler.set(v.image);
+  });
+})(window.popmotion);
